19c3731cb75558485d78406c8c23d4ba1b3eabe7
Yohann Jolain <yohann.jolain@reboot-conseil.com>
2021-12-06 14:32:47 +0100

    Added day 1 and 2

---
+++ day1/main.py
@@ -0,0 +1,31 @@
+sample = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+def readData():
+    f = open("./data.txt", "r")
+    values = list(map(lambda x: int(x.replace("\n", "")), f.readlines()))
+    return values
+
+def getIncreases(lst): 
+    increases = 0;
+    prev = lst[0]
+    for i in range(1, len(lst)):
+        curr = lst[i]
+        if curr > prev:
+            increases += 1
+        prev = curr
+    return increases
+
+def getSlidingSum(lst, factor):
+    result = []
+    for i in range(len(lst) - factor +1):
+        sum = 0
+        for j in range(factor):
+            sum += lst[i+j]
+        result.append(sum)
+    return result
+
+def getSlidingIncreases(lst, factor):
+    slidingSum = getSlidingSum(lst, factor)
+    return getIncreases(slidingSum)
+
+print(getSlidingIncreases(readData(), 3))
---
+++ day2/main.py
@@ -0,0 +1,53 @@
+from typing import List
+
+sample = [["forward", "5"], ["down", "5"], ["forward", "8"], ["up", "3"], ["down", "8"], ["forward", "2"]]
+
+class Command: 
+    def __init__(self, commandLine):
+        self.command = commandLine[0]
+        self.step = int(commandLine[1])
+    
+    def isForward(self):
+        return self.command == "forward"
+
+    def isUp(self):
+        return self.command == "up"
+
+    def isDown(self):
+        return self.command == "down"
+
+sample = list(map(lambda x: Command(x), sample))
+
+def readData() -> List[Command]:
+    f = open("./data.txt", "r")
+    values = list(map(lambda x: Command(x.replace("\n", "").split()), f.readlines()))
+    return values
+
+def getCoordinates(commands: List[Command]):
+    depth = 0
+    horizontal = 0
+    for com in commands:
+        if com.isForward(): 
+            horizontal += com.step
+        if com.isUp():
+            depth -= com.step
+        if com.isDown():
+            depth += com.step
+    return horizontal * depth
+
+def getFinalCoordinates(commands: List[Command]):
+    aim = 0
+    depth = 0
+    horizontal = 0
+    for com in commands:
+        if com.isForward(): 
+            horizontal += com.step
+            depth += aim * com.step
+        if com.isUp():
+            aim -= com.step
+        if com.isDown():
+            aim += com.step
+    return horizontal * depth
+
+print(getFinalCoordinates(sample))
+print(getFinalCoordinates(readData()))
